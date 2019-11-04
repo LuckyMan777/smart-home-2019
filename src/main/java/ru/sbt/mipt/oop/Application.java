6f2894a5ec5c13224c5b2da050b33c612f273fac
Aleksandr Rudnev <rud.sasha1997@gmail.com>
2019-11-04 23:19:48 +0300

    Update Application.java

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -3,9 +3,6 @@ package ru.sbt.mipt.oop;
 import com.coolcompany.smarthome.events.SensorEventsManager;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
-import ru.sbt.mipt.oop.eventproviders.SensorEventRandomProvider;
-import ru.sbt.mipt.oop.homeproviders.SmartHomeJSONProvider;
-import ru.sbt.mipt.oop.homeproviders.SmartHomeProvider;
 
 public class Application {
 
